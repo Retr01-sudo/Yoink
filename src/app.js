b2405fa032082d0c994d82a513a639f109918eb3
Retr01-sudo <retrowrench.06@gmail.com>
2026-02-19 00:34:06 +0530

    added routes and middleware for /metrics in app.js

--- src/app.js
+++ src/app.js
@@ -2,15 +2,19 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import { helmetOptions, corsOptions } from "./config/security.js";
-import healthRoute from "./routes/health.js";
+import healthRoute from "./routes/health.route.js";
 import errorHandler from "./middleware/errorHandler.js";
 import dotenv from "dotenv";
+import { trackLatency } from "./middleware/metrics.middleware.js";
+import metricsRoute from "./routes/metrics.route.js";
 
 dotenv.config();
 
 const app = express();
 
 const isDev = process.env.NODE_ENV !== "production";
+
+app.use(trackLatency);
 
 app.use(express.json({ limit: "10mb" }));
 
@@ -21,6 +25,8 @@ app.use(helmet(helmetOptions));
 app.use(cors(corsOptions));
 
 app.options("/{*path}", cors(corsOptions));
+
+app.use("/metrics", metricsRoute)
 
 app.use("/health", healthRoute);
 
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
