8dadaec5274a6cc2460d6281858203d76403efd1
Retr01-sudo <retrowrench.06@gmail.com>
2026-02-19 00:26:02 +0530

    fix: changed the order of .connect() and removed await because it was causing dead hang in server

--- src/config/redis.js
+++ src/config/redis.js
@@ -7,12 +7,12 @@ const redisClient = redis.createClient({
     url: process.env.REDIS_URL,
 });
 
-await redisClient.connect();
-
 redisClient.on('error', (err) => console.log('Redis Client Error:', err));
 
 redisClient.on('connect', () => console.log('Connecting to Redis...'));
 
 redisClient.on('ready', () => console.log('Redis is live and ready!'));
+
+redisClient.connect();
 
 export { redisClient };
