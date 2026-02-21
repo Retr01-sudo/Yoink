78949a116ab75b56448018b8714c44e57e2dcc97
Retr01-sudo <retrowrench.06@gmail.com>
2026-02-21 23:41:40 +0530

    Added code for exporting data required by k6

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,6 +1,7 @@
 import { randomUUID } from "crypto";
 import { prisma } from "../src/config/db.js";
-
+import { fs } from "fs";
+ 
 
 const cleanup = async () => {
     console.log("Cleaning up old data...");
@@ -59,7 +60,7 @@ const addProducts = async (numberOfProducts) => {
 const seed = async () => {
     try {
         await cleanup();
-        const numberOfUsers=10;
+        const numberOfUsers=500;
         console.log("Seeding users...");
         await addUsers(numberOfUsers);
         console.log("Users seeded successfully");
@@ -67,6 +68,43 @@ const seed = async () => {
         console.log("Seeding products...");
         await addProducts(10);
         console.log("Products seeded successfully");
+
+        // --- k6 Export Logic Start ---
+        console.log("Exporting IDs for k6 spike testing...");
+
+        // Fetch the inserted data from the database
+        const users = await prisma.user.findMany({ select: { id: true } });
+        // const products = await prisma.product.findMany({ 
+        //     select: { id: true, name: true, stock: true } 
+        // });
+        const product = await prisma.product.findFirst({
+            where: {
+                name: "Mechanical Keyboard"
+            },
+            select:
+            {
+                id: true,
+                name: true,
+                stock: true
+            }
+        })
+
+        const k6Data = {
+            userIds: users.map(u => u.id),
+            product: product
+            // products: products // Exporting full product details to make selection easier in k6
+        };
+
+        // Ensure the benchmarks/k6 directory exists before writing
+        const dirPath = path.resolve('./benchmarks/k6');
+        if (!fs.existsSync(dirPath)) {
+            fs.mkdirSync(dirPath, { recursive: true });
+        }
+
+        const filePath = path.join(dirPath, 'data.json');
+        fs.writeFileSync(filePath, JSON.stringify(k6Data, null, 2));
+        
+        console.log(`Successfully exported ${users.length} users and product with name:${product.name} and stock: ${product.stock} to ${filePath}`);
     }
     catch (err) {
         throw err;
